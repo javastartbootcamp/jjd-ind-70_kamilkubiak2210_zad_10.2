038ee0107f469418f747ab2ed04c801d40cce082
kamilkubiak2210 <kamil.kubiak4@outlook.com>
2023-05-10 21:10:38 +0200

    Poprawka5

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -7,34 +7,30 @@ public class Main {
     public static void main(String[] args) {
         Phone phone = new Phone(new MixPhoneContract(.5, .1, .2, .5,
                 1, 1, 3));
-//
-//        phone.printAccountState();
-//
-//        phone.sendSms();
-//
-//        phone.printAccountState();
-//
-//        phone.call(90);
-//
-//        phone.printAccountState();
-//
-//        phone.sendSms();
-//
-//        phone.printAccountState();
-//
-//        phone.call(70);
-//
-//        phone.printAccountState();
-//
-//        phone.sendMms();
-//
-//        phone.printAccountState();
-//
-//        phone.sendMms();
-//
-//        phone.printAccountState();
 
-        phone.call(300);
+        phone.printAccountState();
+
+        phone.sendSms();
+
+        phone.printAccountState();
+
+        phone.call(90);
+
+        phone.printAccountState();
+
+        phone.sendSms();
+
+        phone.printAccountState();
+
+        phone.call(70);
+
+        phone.printAccountState();
+
+        phone.sendMms();
+
+        phone.printAccountState();
+
+        phone.sendMms();
 
         phone.printAccountState();
     }
